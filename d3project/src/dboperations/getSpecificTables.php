1c717e3b165faa2230cdcfedb678951b766ef3fd
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2021-05-05 17:25:14 +0300

    Add files via upload

---
+++ d3project/src/dboperations/getSpecificTables.php
@@ -0,0 +1,65 @@
+<?php
+require_once "../controllers/controller.php";
+require_once "../constants/DBconstants.php";
+
+//Travel services (%of service imports)
+$SESid=$_SESSION['id'];
+$countryArray=$_SESSION['country'];
+if($conn->connect_error){
+    die('Database error:' .$conn->connect_error);
+}
+ 
+    if (mysqli_connect_error()){
+    die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
+    }else{
+    //$result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND `CountryCode` IN (" . implode(',', array_map('intval', $countryArray)) . ")");
+        switch(count($countryArray)){
+            case 1:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]')");
+                break;
+            case 2:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]' )");
+                break;
+            case 3:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]' OR `CountryCode`='$countryArray[2]')");
+                break;
+            case 4:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]' OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]')");
+                break;
+            case 5:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]' OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]')");
+                break;
+            case 6:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]'  OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]' OR `CountryCode`='$countryArray[5]')");
+                break;
+            case 7:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]'  OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]' OR `CountryCode`='$countryArray[5]'OR `CountryCode`='$countryArray[6]')");
+                break;
+            case 8:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]'  OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]' OR `CountryCode`='$countryArray[5]'OR `CountryCode`='$countryArray[6]' OR `CountryCode`='$countryArray[7]')");
+                break;
+            case 9:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]'  OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]' OR `CountryCode`='$countryArray[5]'OR `CountryCode`='$countryArray[6]' OR `CountryCode`='$countryArray[7]' OR `CountryCode`='$countryArray[8]')");
+                break;
+            default:
+                $result = mysqli_query($conn, "SELECT * FROM `countrycodeindicatorcodeyears` WHERE IndicatorCode='$SESid' AND (`CountryCode`='$countryArray[0]' OR `CountryCode`='$countryArray[1]'  OR `CountryCode`='$countryArray[2]' OR `CountryCode`='$countryArray[3]' OR `CountryCode`='$countryArray[4]' OR `CountryCode`='$countryArray[5]'OR `CountryCode`='$countryArray[6]' OR `CountryCode`='$countryArray[7]' OR `CountryCode`='$countryArray[8]' OR `CountryCode`='$countryArray[9]')");
+                break;
+        }
+
+    $data = array();
+    while ($row = mysqli_fetch_object($result))
+    {
+    array_push($data, $row);
+    }
+    
+    echo json_encode($data);
+     exit();
+    $conn->close();
+    }
+    
+?>
+
+   
+
+
+
